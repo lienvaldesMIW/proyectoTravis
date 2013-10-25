e08c51cf1035f64738ed3bdc0f7baf703d8d9d6d
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-25 20:10:43 +0200

    commit travis

---
+++ src/Suma.java
@@ -0,0 +1,8 @@
+public class Suma {
+	public double getSuma(double operando1, double operando2) {
+		return operando1 + operando2;
+	}
+	public double incrementa(double operando) {
+		return operando + 1;
+	}
+}
